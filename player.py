dc24eebb8a4ee7690be663c16530fb00933966ce
Paul <paul.buys1@gmail.com>
2022-12-19 19:11:57 +0200

    lesson_1

--- player.py
+++ player.py
@@ -1,17 +1,29 @@
 import pygame, sys
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos):
+    def __init__(self, pos, constraint, speed):
         super().__init__()
         self.image = pygame.image.load('./graphics/player.png').convert_alpha()
         self.rect = self.image.get_rect(midbottom = pos)
-        self.speed = 5
+        self.speed = speed
+        self.max_x_constraint = constraint
     def get_input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_RIGHT]:
             self.rect.x += self.speed
         elif keys[pygame.K_LEFT]:
             self.rect.x -= self.speed
+        if keys[pygame.K_SPACE]:
+            self.shoot_laser()
 
     def update(self):
         self.get_input()
+        self.constrait()
+    def constrait(self):
+        if self.rect.left <= 0:
+            self.rect.left = 0
+        if self.rect.right >= self.max_x_constraint:
+            self.rect.right = self.max_x_constraint
+
+    def shoot_laser(self):
+        print("Shots fired")
