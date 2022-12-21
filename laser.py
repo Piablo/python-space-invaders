4b9281b592ff0c80229d47ff74dace8f9b14681e
Paul <paul.buys1@gmail.com>
2022-12-21 08:55:01 +0200

    lesson_3

--- laser.py
+++ laser.py
@@ -4,12 +4,12 @@ class Laser(pygame.sprite.Sprite):
     def __init__(self, pos, speed, screen_height):
         super().__init__()
         self.image = pygame.Surface((4, 20))
-        self.image.fill('white')
+        self.image.fill('#00ff00')
         self.rect = self.image.get_rect(center = pos)
         self.speed = speed
         self.height_y_constraint = screen_height
     def destroy(self):
-        if self.rect.y <= -50 or self.rect.y >= self.height_y_constraint +50:
+        if self.rect.y <= -50 or self.rect.y >= self.height_y_constraint + 50:
             self.kill()
     def update(self):
         self.rect.y += self.speed
